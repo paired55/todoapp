4520d4ca2aab6562f9320a565be914f255a01efb
Salah Ahmed <riddenariel@gmail.com>
2024-03-01 02:57:03 +0200

    project done will implement localStorage later

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const modal = document.querySelector("#modal");
+const taskForm = document.querySelector("#taskForm");
+const addButton = document.querySelector("#addButton");
+const closeButton = document.querySelector("#closeButton");
+const hero = document.querySelector("#hero");
+
+addButton.addEventListener("click", () => {
+	modal.show();
+});
+
+closeButton.addEventListener("click", () => {
+	modal.close();
+});
+
+taskForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+	modal.close();
+	const formData = {
+		title: document.querySelector("#title").value,
+		description: document.querySelector("#description").value,
+		dueDate: document.querySelector("#dueDate").value,
+	};
+
+	const card = document.createElement("div");
+	const taskContainer = document.createElement("ul");
+	const taskItemOne = document.createElement("li");
+	const taskItemTwo = document.createElement("li");
+	const taskItemThree = document.createElement("li");
+	const removeButton = document.createElement("button");
+
+	card.classList.add("card");
+
+	taskItemOne.textContent = `Task: ${formData.title}`;
+	taskItemTwo.textContent = `Description: ${formData.description}`;
+	taskItemThree.textContent = `Due Date: ${formData.dueDate}`;
+	removeButton.textContent = "Done!";
+	removeButton.style.cssText =
+		"background-color: #eeeeee; border: none; border-radius: 15px; padding: 3px 5px 3px 5px; width: 50%";
+
+	removeButton.addEventListener("click", () => {
+		card.remove();
+	});
+
+	taskContainer.appendChild(taskItemOne);
+	taskContainer.appendChild(taskItemTwo);
+	taskContainer.appendChild(taskItemThree);
+	card.appendChild(taskContainer);
+	card.appendChild(removeButton);
+	hero.appendChild(card);
+});
